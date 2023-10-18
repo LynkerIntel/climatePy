62b6686afc67137e0ce0a6651a0ce7a8c6ae2b4d
anguswg-ucsb <anguswatters@gmail.com>
2023-10-17 17:56:40 -0700

    fixing incorrect license name in setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),    # python modules to install
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3.0",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],          
     python_requires='>=3.6',               # min python version required 
